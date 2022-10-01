01711bc329f655840b4a8491a7f364fc431f1745
gabriellengoo <gabriellengoo@hotmail.com>
2022-10-01 19:56:47 +0100

    init

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const articleRouter = require('./routes/articles');
 // const aboutRouter = require('./routes/about');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 
 // we are useing the public folder
